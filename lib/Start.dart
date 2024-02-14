9f644f8e794a7f5c4eb43fc09837c86ae9fe1e7c
Mohebullah Jami <91026012+Mohebjami@users.noreply.github.com>
2024-02-14 22:06:58 +0430

    Initial commit

--- lib/Start.dart
+++ lib/Start.dart
@@ -4,20 +4,52 @@ import 'package:balanceballers/Register.dart';
 import 'package:flutter/material.dart';
 
 class Start extends StatefulWidget {
-  const Start({super.key});
+  final Function toggleTheme;
+
+  const Start({Key? key,  required this.toggleTheme}) : super(key: key);
 
   @override
   State<Start> createState() => _StartState();
 }
 
 class _StartState extends State<Start> {
-    @override
+  get _isLightTheme => true;
+
+  @override
   Widget build(BuildContext context) {
     double fullScreenHeight = MediaQuery.of(context).size.height;
     double fullScreenWidth = MediaQuery.of(context).size.width;
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Balance Ballers"),
+        title: Builder(
+          builder: (context) => GestureDetector(
+            child: const CircleAvatar(
+              backgroundImage: AssetImage("assets/football.png"),
+              radius: 20.0,
+            ),
+            onTap: () {},
+          ),
+        ),
+        actions: <Widget>[
+          SizedBox(
+            width: 70,
+            height: 40,
+            child: FittedBox(
+              child: Switch(
+                value: _isLightTheme,
+                onChanged: (value) {
+                  widget.toggleTheme();
+                },
+                activeColor: Colors.yellow,
+                inactiveThumbColor: Colors.blueGrey,
+                inactiveTrackColor: Colors.grey,
+                activeTrackColor: Colors.orange,
+                activeThumbImage: const AssetImage('assets/sun.png'),
+                inactiveThumbImage: const AssetImage('assets/moon.png'),
+              ),
+            ),
+          ),
+        ],
       ),
       body: SizedBox(
         height: fullScreenHeight,
@@ -51,6 +83,4 @@ class _StartState extends State<Start> {
       ),
     );
   }
-
-  
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,58 @@
 import 'package:balanceballers/Register.dart';
-import 'package:balanceballers/Start.dart';
+import 'package:balanceballers/firebase_options.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
+import 'package:balanceballers/Start.dart'; // Import the Start class from start.dart
+import 'dart:io';
 
-void main() async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  runApp(MyApp());
+}
 
-  runApp(
-      MaterialApp(
-        debugShowCheckedModeBanner: false,
-        initialRoute: 'start',
-        routes: {
-          'start': (context) => const Start(),
-          'Register': (context) => const Register(),
-        },
-      ));
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool? _isLightTheme = true;
+
+  void toggleTheme() {
+    setState(() {
+      _isLightTheme = !_isLightTheme!;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      themeMode: _isLightTheme! ? ThemeMode.light : ThemeMode.dark,
+      theme: ThemeData(
+        brightness: Brightness.light,
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          titleTextStyle: TextStyle(color: Colors.black),
+        ),
+      ),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          titleTextStyle: TextStyle(color: Colors.white),
+        ),
+      ),
+      initialRoute: 'start',
+      routes: {
+        'start': (context) => Start(toggleTheme: toggleTheme),
+        'Register': (context) => const Register(),
+      },
+    );
+  }
 }
