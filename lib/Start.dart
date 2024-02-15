deae67150112612c479b1c8ac88a164b08b9d483
Mohebullah Jami <91026012+Mohebjami@users.noreply.github.com>
2024-02-16 01:38:22 +0430

    Initial commit

--- lib/Start.dart
+++ lib/Start.dart
@@ -6,25 +6,37 @@ import 'package:flutter/material.dart';
 class Start extends StatefulWidget {
   final Function toggleTheme;
 
-  const Start({Key? key,  required this.toggleTheme}) : super(key: key);
+  const Start({Key? key, required this.toggleTheme}) : super(key: key);
 
   @override
   State<Start> createState() => _StartState();
 }
 
 class _StartState extends State<Start> {
-  get _isLightTheme => true;
+  bool _isLightTheme = false; // Declare _isLightTheme
+
+  bool get isLightTheme => _isLightTheme; // Getter for _isLightTheme
+
+  set isLightTheme(bool value) { // Setter for _isLightTheme
+    setState(() {
+      _isLightTheme = value;
+    });
+  }
+
 
   @override
   Widget build(BuildContext context) {
     double fullScreenHeight = MediaQuery.of(context).size.height;
     double fullScreenWidth = MediaQuery.of(context).size.width;
+
     return Scaffold(
+      backgroundColor: _isLightTheme ? Color.fromRGBO(255, 255, 240, 1) : Color.fromRGBO(10, 23, 42,1),
       appBar: AppBar(
         title: Builder(
           builder: (context) => GestureDetector(
             child: const CircleAvatar(
-              backgroundImage: AssetImage("assets/football.png"),
+              backgroundImage: AssetImage("assets/pic/football.png"),
+              backgroundColor: Colors.transparent,
               radius: 20.0,
             ),
             onTap: () {},
@@ -38,14 +50,17 @@ class _StartState extends State<Start> {
               child: Switch(
                 value: _isLightTheme,
                 onChanged: (value) {
-                  widget.toggleTheme();
+                  setState(() {
+                    _isLightTheme = value;
+                    widget.toggleTheme();
+                  });
                 },
                 activeColor: Colors.yellow,
                 inactiveThumbColor: Colors.blueGrey,
                 inactiveTrackColor: Colors.grey,
                 activeTrackColor: Colors.orange,
-                activeThumbImage: const AssetImage('assets/sun.png'),
-                inactiveThumbImage: const AssetImage('assets/moon.png'),
+                activeThumbImage: const AssetImage('assets/pic/sun.png'),
+                inactiveThumbImage: const AssetImage('assets/pic/moon.png'),
               ),
             ),
           ),
@@ -55,28 +70,88 @@ class _StartState extends State<Start> {
         height: fullScreenHeight,
         width: fullScreenWidth,
         child: Padding(
-          padding: const EdgeInsets.all(8.0),
+          padding: const EdgeInsets.only(left: 13.0, top: 0,right: 13.0),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              ElevatedButton(onPressed: (){
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const Register()),
-                );
-              }, child: const Text("Register")),
-              ElevatedButton(onPressed: (){
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) =>  PlayerList()),
-                );
-              }, child: const Text("Players")),
-              ElevatedButton(onPressed: (){
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) =>  Debtors()),
-                );
-              }, child: const Text("Debtors")),
+               Text(
+                "Herat Futsal Club ",
+                style: TextStyle(fontSize: 40, fontFamily: "ProtestGuerrilla",color: _isLightTheme ? const Color.fromRGBO(0, 0, 0, 1) : Color.fromRGBO(255, 255, 255, 1),),
+              ),
+              const SizedBox(
+                height: 60,
+              ),
+               Center(
+                child: Container(
+                  height: 200,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(image: AssetImage("assets/pic/Ball.png"),fit: BoxFit.contain)
+                  ),
+                 
+                ),
+              ),
+              const SizedBox(
+                height: 60,
+              ),
+              Center(
+                child: SizedBox(
+                  width: fullScreenWidth-120,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      SizedBox(
+                        height: 50,
+                        child: ElevatedButton(
+                            style: const ButtonStyle(
+                                backgroundColor: MaterialStatePropertyAll(
+                                    Color.fromRGBO(255, 180, 0, 1))),
+                            onPressed: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(builder: (context) => const Register()),
+                              );
+                            },
+                            child: const Text("Register" , style: TextStyle(fontSize: 20,fontFamily: "TrajanPro", fontWeight: FontWeight.bold),)),
+                      ),
+                      SizedBox(
+                        height: 15,
+                      ),
+                      SizedBox(
+                        height: 50,
+                        child: ElevatedButton(
+                            style: const ButtonStyle(
+                                backgroundColor: MaterialStatePropertyAll(
+                                    Color.fromRGBO(255, 180, 0, 1))),
+                            onPressed: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(builder: (context) => PlayerList()),
+                              );
+                            },
+                            child: const Text("Players", style: TextStyle(fontSize: 20,fontFamily: "TrajanPro", fontWeight: FontWeight.bold),)),
+                      ),
+                      SizedBox(
+                        height: 15,
+                      ),
+                      SizedBox(
+                        height: 50,
+                        child: ElevatedButton(
+                            style: const ButtonStyle(
+                                backgroundColor: MaterialStatePropertyAll(
+                                    Color.fromRGBO(255, 180, 0, 1))),
+                            onPressed: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(builder: (context) => Debtors()),
+                              );
+                            },
+                            child: const Text("Debtors", style: TextStyle(fontSize: 20,fontFamily: "TrajanPro", fontWeight: FontWeight.bold),)),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
             ],
           ),
         ),
